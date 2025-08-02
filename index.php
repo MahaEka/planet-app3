65b1a5c447a87bb00f62c563aa0390827b3ed4c9
Mahabat Erkalieva <flysof777@gmail.com>
2025-08-02 21:34:13 +0000

    Initial commit - Planet App project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Planet App</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Welcome to the Planet App</h1>
+    <p><a href="planets.php">View Planets</a></p>
+</body>
+</html>
---
+++ planets.php
@@ -0,0 +1,8 @@
+<?php
+$planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+echo "<h1>Planets in our Solar System</h1><ul>";
+foreach($planets as $planet){
+    echo "<li>$planet</li>";
+}
+echo "</ul>";
+?>
